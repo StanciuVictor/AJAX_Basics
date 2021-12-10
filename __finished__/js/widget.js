6419d32a99b7973100664227a7c4d2d6697cbcad
StanciuVictor <victor.stanciu@yahoo.com>
2021-12-11 00:35:59 +0200

    Format code and uncommented personal code

--- __finished__/js/widget.js
+++ __finished__/js/widget.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', '../data/employees.json');
 xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4 && xhr.status === 200) {
+  if (xhr.readyState === 4 && xhr.status === 200) {
     var employees = JSON.parse(xhr.responseText);
     var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i += 1) {
+    for (var i = 0; i < employees.length; i += 1) {
       if (employees[i].inoffice === true) {
         statusHTML += '<li class="in">';
       } else {
--- project/js/widget.js
+++ project/js/widget.js
@@ -1,42 +1,16 @@
-// // Create XHR object
-// const xhr = new XMLHttpRequest();
+// Create XHR object
+const xhr = new XMLHttpRequest();
 
-// // Create callback function
-// xhr.onreadystatechange = function () {
-//   // If client received response from server
-//   if (xhr.readyState === 4) {
-//     // Returns a JS object
-//     const employees = JSON.parse(xhr.responseText);
-
-//     // Create widget content
-//     let statusHTML = '<ul class="bulleted">';
-//     for (let i = 0; i < employees.length; i++) {
-//       if (employees[i].inoffice === true) {
-//         statusHTML += '<li class="in">';
-//       } else {
-//         statusHTML += '<li class="out">';
-//       }
-//       statusHTML += employees[i].name;
-//       statusHTML += '</li>';
-//     }
-//     statusHTML += '</ul>';
-//     document.getElementById('employeeList').innerHTML = statusHTML;
-//     console.log(statusHTML);
-//   }
-// };
-// // Open request - GET - load file
-// // In realiti, this points to a server side script that would
-// // dynamically generate this JSON data with the most current info on employees
-// xhr.open('GET', 'data/employees.json');
-// xhr.send();
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'data/employees.json');
+// Create callback function
 xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i = 0; i < employees.length; i += 1) {
+  // If client received response from server
+  if (xhr.readyState === 4) {
+    // Returns a JS object
+    const employees = JSON.parse(xhr.responseText);
+
+    // Create widget content
+    let statusHTML = '<ul class="bulleted">';
+    for (let i = 0; i < employees.length; i++) {
       if (employees[i].inoffice === true) {
         statusHTML += '<li class="in">';
       } else {
@@ -47,6 +21,11 @@ xhr.onreadystatechange = function () {
     }
     statusHTML += '</ul>';
     document.getElementById('employeeList').innerHTML = statusHTML;
+    console.log(statusHTML);
   }
 };
+// Open request - GET - load file
+// In realiti, this points to a server side script that would
+// dynamically generate this JSON data with the most current info on employees
+xhr.open('GET', 'data/employees.json');
 xhr.send();
