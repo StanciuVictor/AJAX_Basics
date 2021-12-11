de01713930587b3c7c40134741813237fdc5cff5
StanciuVictor <victor.stanciu@yahoo.com>
2021-12-11 16:23:52 +0200

    Add available rooms sidebar

--- project/js/widget.js
+++ project/js/widget.js
@@ -1,32 +1,64 @@
 // Create XHR object
-const xhr = new XMLHttpRequest();
+const xhrEmployees = new XMLHttpRequest();
 
 // Create callback function
-xhr.onreadystatechange = function () {
+xhrEmployees.onreadystatechange = function () {
   // If client received response from server
-  if (xhr.readyState === 4 && xhr.status === 200) {
+  if (xhrEmployees.readyState === 4 && xhrEmployees.status === 200) {
     // Returns a JS array of objects. The employees.json, but JS format
-    const employees = JSON.parse(xhr.responseText);
+    const employees = JSON.parse(xhrEmployees.responseText);
     // console.log(employees);
 
     // Create widget content
-    let statusHTML = '<ul class="bulleted">';
+    let employeesStatusHTML = '<ul class="bulleted">';
     for (let i = 0; i < employees.length; i++) {
       if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
+        employeesStatusHTML += '<li class="in">';
       } else {
-        statusHTML += '<li class="out">';
+        employeesStatusHTML += '<li class="out">';
       }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
+      employeesStatusHTML += employees[i].name;
+      employeesStatusHTML += '</li>';
     }
-    statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-    console.log(statusHTML);
+    employeesStatusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = employeesStatusHTML;
   }
 };
+
 // Open request - GET - load file
-// In realiti, this points to a server side script that would
+// In reality, this points to a server side script that would
 // dynamically generate this JSON data with the most current info on employees
-xhr.open('GET', 'data/employees.json');
-xhr.send();
+xhrEmployees.open('GET', 'data/employees.json');
+xhrEmployees.send();
+
+// Create XHR object
+const xhrRooms = new XMLHttpRequest();
+
+// Create callback function
+xhrRooms.onreadystatechange = function () {
+  // If client received response from server
+  if (xhrRooms.readyState === 4 && xhrRooms.status === 200) {
+    // Returns a JS array of objects. The employees.json, but JS format
+    const rooms = JSON.parse(xhrRooms.responseText);
+
+    // Create widget content
+    let roomStatusHTML = '<ul class="rooms">';
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].available === true) {
+        roomStatusHTML += '<li class="full">';
+      } else {
+        roomStatusHTML += '<li class="empty">';
+      }
+      roomStatusHTML += rooms[i].room;
+      roomStatusHTML += '</li>';
+    }
+    roomStatusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = roomStatusHTML;
+  }
+};
+
+// Open request - GET - load file
+// In reality, this points to a server side script that would
+// dynamically generate this JSON data with the most current info on rooms
+xhrRooms.open('GET', 'data/rooms.json');
+xhrRooms.send();
